76524872736c9e5025c086da9441c93f10e3c997
Uzzy97 <g00345816@gmit.ie>
2018-11-20 00:14:12 +0000

    Drop Down Menu

--- src/app/post-create/post-create.component.ts
+++ src/app/post-create/post-create.component.ts
@@ -1,13 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+//import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms";
 import {PostService} from '../services/post.services';
 
-@Component({
+/*@Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent implements OnInit {
+
+  constructor(private service:PostService) { }
+
+  onAddPost(form: NgForm) {
+
+    this.service.addPost(form.value.title, form.value.content).subscribe();
+    
+    console.log(form.value);
+    form.resetForm();
+  }
+  */
+
+ import {Component,  OnInit } from '@angular/core';
+
+ /**
+  * @title Basic expansion panel
+  */
+ @Component({
+   selector: 'app-post-create',
+   templateUrl: './post-create.component.html',
+   styleUrls: ['./post-create.component.css'],
+ })
+ //export class PostCreateComponent {
+   //panelOpenState = false;
+ //}
+
+ export class PostCreateComponent implements OnInit {
 
   constructor(private service:PostService) { }
 
@@ -24,6 +51,6 @@ export class PostCreateComponent implements OnInit {
 
 
 
-  }
+ }
 
 }
